dbfd8679c5bb1ccda0108e0ed27b451645f7e4c6
Anurag Gautam <ngrbrd@gmail.com>
2018-12-23 12:33:33 +0530

    exercise 1.4

---
+++ chapter_1/exercise_1_4.c
@@ -0,0 +1,20 @@
+/* Celsius to Fahrenheit conversion */
+
+#include <stdio.h>
+
+int main() {
+  float fahr, celsius;
+  float lower, upper, step;
+  lower = 0;
+  upper = 300;
+  step = 20;
+
+  celsius = lower;
+
+  printf("  C      F\n");
+  while(celsius <= upper) {
+    fahr = (9.0 * celsius) / 5.0 + 32;
+    printf("%3.0f %6.1f\n", celsius, fahr);
+    celsius += step;
+  }
+}
